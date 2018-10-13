42439a5485b5489a04ada0cd9cd7299c207fc9b7
Gavin Meier <gavinmeier41910@gmail.com>
2018-10-13 15:17:02 -0500

    pulled up findOneUser

--- src/schema/query.js
+++ src/schema/query.js
@@ -21,12 +21,20 @@ const Query = new GraphQLObjectType({
                 return UserModel.find({})
             }
         },
+        findOneUser: {
+            type: UserType,
+            args: {
+              id: { type: GraphQLID }
+            },
+            resolve(parentValue, {id}){
+                return UserModel.findById(id)
+        },
         findAllCompanies : {
             type: new GraphQLList(CompanyType),
             resolve() {
                 return CompanyModel.find({})
             }
-        }
+    }}
     })
 })
 
